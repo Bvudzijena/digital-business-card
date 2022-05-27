c554e401e5cb77570447ae71e05a12e642d9ca94
Darlington Bvudzijena <darlingtonbvudzijena@gmail.com>
2022-05-27 14:12:20 +0200

    "app repairs after App.js crush down"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
-function App() {
+export default function App() {
+
+    const Navbar() = (
+      <div>
+
+      </div>
+    )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar />
+      <h1>Darlington Bvudzijena</h1>
+      <h2>Front End Developer</h2>
+      <button>Email</button>
+      <button>LinkedIn</button>
+      <div>
+        <h3>About</h3>
+        <pre>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+          I try to keep up with security and best practices, and am always looking for new things to learn.
+        </pre>
+      </div>
+      <div>
+        <h3>Interests</h3>
+        <pre>Food expert.
+           Music scholar. 
+           Reader. 
+           Internet fanatic. 
+           Bacon buff. 
+           Entrepreneur. 
+           Travel geek. 
+           Pop culture ninja. 
+           Coffee fanatic.
+          </pre>
+      </div>
+      </div>
   );
 }
 
-export default App;
