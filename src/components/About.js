8443957eb7a8e36038e1c344ed8ac6ce076715b7
Darlington Bvudzijena <darlingtonbvudzijena@gmail.com>
2022-05-30 14:52:03 +0200

    New Changes on other components

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function About () {
+    return (
+        <div>
+            <div>
+<h3>About</h3>
+<pre>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+  I try to keep up with security and best practices, and am always looking for new things to learn.
+</pre>
+</div>
+<div>
+<h3>Interests</h3>
+<pre>Food expert.
+   Music scholar. 
+   Reader. 
+   Internet fanatic. 
+   Bacon buff. 
+   Entrepreneur. 
+   Travel geek. 
+   Pop culture ninja. 
+   Coffee fanatic.
+  </pre>
+</div>
+        </div>
+    )
+}
+
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>Darlington Bvudzijena</h1>
+            <h2>Front End Developer</h2>
+            <button>LinkedIn</button>
+            <button>Email</button>
+        </div>
+    )
+}
