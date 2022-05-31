d0b6af873969a4f30f58e9799e45c32e8140db3d
Darlington Bvudzijena <darlingtonbvudzijena@gmail.com>
2022-05-31 10:46:10 +0200

    changes made to about css on App.css

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,25 @@ export default function About() {
     return (
         <div>
             <div>
-<h3>About</h3>
-<pre>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
-  I try to keep up with security and best practices, and am always looking for new things to learn.
-</pre>
-</div>
-<div>
-<h3>Interests</h3>
-<pre>Food expert.
-   Music scholar. 
-   Reader. 
-   Internet fanatic. 
-   Bacon buff. 
-   Entrepreneur. 
-   Travel geek. 
-   Pop culture ninja. 
-   Coffee fanatic.
-  </pre>
-</div>
-        </div>
+            <h3 className="sub-headings">About</h3>
+              <pre className="about-text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+                  I try to keep up with security and best practices, and am always looking for new things to learn.
+              </pre>
+            </div>
+            <div>
+              <h3 className="sub-headings">Interests</h3>
+              <pre className="about-text">Food expert.
+                Music scholar. 
+                Reader. 
+                Internet fanatic. 
+                Bacon buff. 
+                Entrepreneur. 
+                Travel geek. 
+                Pop culture ninja. 
+                Coffee fanatic.
+              </pre>
+            </div>
+            </div>
     )
 }
 
