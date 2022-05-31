a6c8ba065a87d4e32221e5b1545e888a462824b5
Darlington Bvudzijena <darlingtonbvudzijena@gmail.com>
2022-05-31 10:34:48 +0200

    css for buttons and nav-img

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,10 +4,12 @@ import '../App.css'
 export default function Navbar() {
     return (
         <div>
-            <h1>Darlington Bvudzijena</h1>
-            <h2>Front End Developer</h2>
-            <button>LinkedIn</button>
-            <button>Email</button>
+            <div className="text-headings">
+                <h1>Darlington Bvudzijena</h1>
+                <h2>Front End Developer</h2>
+            </div>
+            <button className="email-button">Email</button>
+            <button className="linkedin-button">LinkedIn</button>
         </div>
     )
 }
